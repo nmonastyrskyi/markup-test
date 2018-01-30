c683a9ca2b7561f6e650020aca71471ec9034e70
Nikita Monastyrskyy <n.monastyrskyi@gmail.com>
2018-01-30 10:56:23 +0200

    finished the site

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
 $(function() {
-
-	// Custom JS
+  $(window).on('resize', function(){
+    if( $(document).width() <= 480 && !$('.team_item').parent().hasClass("block-flexible") ) {
+  $('.team_item:lt(2)').wrapAll('<div class="block-flexible"></div>')
+  $('.team_item.right').wrapAll('<div class="block-flexible"></div>')
+  }
+  else if( $(document).width() > 480) {
+       $('.team_item').unwrap('.block-flexible')
+  }
+  })
+  
 
 });
